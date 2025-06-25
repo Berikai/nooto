9c6ae01d4f62ef816586200acbdc430ee495db98
Berikai <berkayeren2009@gmail.com>
2025-06-25 07:12:40 +0300

    Add github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,9 +6,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: false
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : ''
+		}
 	},
-	  // Disable accessibility warnings 
+	// Disable accessibility warnings 
 	onwarn: (warning, handler) => {
 		if (warning.code.includes("a11y")) return;
 		handler(warning);
